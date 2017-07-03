9815f42285ee5b2da4c4b5e89ea48b6d12260116
Scott Chapman <scottchapman@us.ibm.com>
2017-07-03 19:25:46 -0400

    Add photoDisplay

--- camera.js
+++ camera.js
@@ -1,7 +1,8 @@
 var Gpio = require('pigpio').Gpio;
 var spawn = require('child_process').spawn;
 var exec = require('child_process').exec;
-var stream;
+var preview;
+var photoDisplay;
 var _ = require('lodash');
 var image = "./images/raw_image.jpg";
 
@@ -57,18 +58,34 @@ button4.on('interrupt', _.debounce(function (level) {
 },1000, { leading: true }));
 
 function startStreaming() {
-	if (!stream || stream.killed) {
+	if (!preview || preview.killed) {
 	  var args = ["-w", "1024", "-h", "600", "-vs", "-t", "999999999", "-tl", "100"];
-	  console.log("Starting streaming");
-	  stream = spawn('raspistill', args);
+	  console.log("Starting preview");
+	  preview = spawn('raspistill', args);
 	}
+}
+
+function startPhotoDisplay() {
+	if (!photoDisplay || photoDisplay.killed) {
+	  var args = ["-T", "1", "-a", image];
+	  console.log("Starting photoDisplay");
+	  photoDisplay = spawn('fbi', args);
+	}
+}
+
+function stopPhotoDisplay() {
+  console.log("Stopping PhotoDisplay...");
+  if (!photoDisplay.killed) {
+  	console.log("Killing photoDisplay");
+  	photoDisplay.kill();
+  }
 }
 
 function takePhoto() {
 	stopStreaming();
-	if (!stream || stream.killed) {
+	if (!preview || preview.killed) {
 	  var args = ["-w", "1024", "-h", "600", "-vs", "-o", image, "-f"];
-	  console.log("Starting streaming");
+	  console.log("Starting preview");
 	  exec('raspistill -w 1024 -h 600 -vs -f -o ' + image, (error,stdout,stderr) => {
 			console.log("Photo taken");
 		});
@@ -76,10 +93,9 @@ function takePhoto() {
 }
 
 function stopStreaming() {
-  console.log("Stopping streaming...");
-  if (!stream.killed) {
-	console.log("Killing stream");
-	stream.kill();
+  console.log("Stopping preview...");
+  if (!preview.killed) {
+  	console.log("Killing preview");
+  	preview.kill();
   }
 }
-
